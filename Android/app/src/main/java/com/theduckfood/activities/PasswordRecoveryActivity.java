adb1803bcdaf4ab0f09da0e6b181ada56c93d45a
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2023-04-03 10:02:01 +0700

    Giao diện Khôi phục mật khẩu

---
+++ Android/app/src/main/java/com/theduckfood/activities/PasswordRecoveryActivity.java
@@ -0,0 +1,34 @@
+package com.theduckfood.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+import com.theduckfood.R;
+import com.theduckfood.databinding.ActivityPasswordRecoveryBinding;
+import com.theduckfood.fragments.EnterEmailFragment;
+
+public class PasswordRecoveryActivity extends AppCompatActivity {
+
+    ActivityPasswordRecoveryBinding binding;
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPasswordRecoveryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(binding.fragmentContainer.getId(), new EnterEmailFragment()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (fragmentManager.getBackStackEntryCount() > 0)
+            fragmentManager.popBackStack();
+        else
+            super.onBackPressed();
+    }
+}
