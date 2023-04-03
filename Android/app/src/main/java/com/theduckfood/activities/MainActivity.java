0df24d40def2f27eb303efbe7075d58aed5f8dd2
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2023-04-03 10:02:27 +0700

    Chỉnh sửa MainActivity (không quan trọng)

--- Android/app/src/main/java/com/theduckfood/activities/MainActivity.java
+++ Android/app/src/main/java/com/theduckfood/activities/MainActivity.java
@@ -1,16 +1,55 @@
 package com.theduckfood.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.theduckfood.R;
+import com.theduckfood.databinding.ActivityMainBinding;
+import com.theduckfood.services.FirebaseCloudMessagingService;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateFCMToken);
+    }
+
+    private void updateFCMToken(String fcmToken) {
+        Log.d("FCM", fcmToken);
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String status = intent.getStringExtra("order_status");
+            binding.txtOrderStatus.setText(status);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver,
+                new IntentFilter("com.theduckfood.UPDATE_ORDER_STATUS"));
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+        super.onPause();
     }
 }
