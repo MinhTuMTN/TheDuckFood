53224d0dbc135337061dbb65e60e34764069b76c
DiDi0703 <110220867+DiDi0703@users.noreply.github.com>
2023-05-10 15:04:36 +0700

    FE: profile_store

---
+++ TheDuckFoodMerchant/app/src/main/java/com/theduckfood/merchant/activities/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.theduckfood.merchant.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.theduckfood.merchant.R;
+
+import java.util.Locale;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
