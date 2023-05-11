3052860b5d89f97c313435b656bc241e4dc0bbaa
DiDi0703 <110220867+DiDi0703@users.noreply.github.com>
2023-05-11 10:19:36 +0700

    FE: trang chi tiết hoá đơn

---
+++ TheDuckFoodMerchant/app/src/main/java/com/theduckfood/merchant/activities/OrderDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.theduckfood.merchant.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.theduckfood.merchant.R;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+}
