a302621338c61da0cc10ece58b037124a7cd5bbf
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2023-05-12 10:31:55 +0700

    FE: Hoàn thành chỉnh sửa giao diện trang theo dõi đơn hàng (Store)

--- TheDuckFoodMerchant/app/src/main/java/com/theduckfood/merchant/fragments/OrdersFragment.java
+++ TheDuckFoodMerchant/app/src/main/java/com/theduckfood/merchant/fragments/OrdersFragment.java
@@ -10,11 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.theduckfood.merchant.R;
+import com.theduckfood.merchant.adapter.ViewOrderAdapter;
 import com.theduckfood.merchant.databinding.FragmentOrdersBinding;
+
 public class OrdersFragment extends Fragment {
     private static OrdersFragment instance;
     private FragmentOrdersBinding binding;
+
     public static OrdersFragment newInstance() {
         return instance == null ? new OrdersFragment() : instance;
     }
@@ -29,5 +34,25 @@ public class OrdersFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ViewOrderAdapter viewOrderAdapter = new ViewOrderAdapter(this);
+        binding.viewPager.setAdapter(viewOrderAdapter);
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager,
+                (tab, position) -> {
+                    switch (position) {
+                        case 0:
+                            tab.setText("Hiện tại");
+                            break;
+                        case 1:
+                            tab.setText("Đang giao");
+                            break;
+                        case 2:
+                            tab.setText("Hoàn thành");
+                            break;
+                        default:
+                            tab.setText("Bị hủy");
+                            break;
+                    }
+                }
+        ).attach();
     }
 }
