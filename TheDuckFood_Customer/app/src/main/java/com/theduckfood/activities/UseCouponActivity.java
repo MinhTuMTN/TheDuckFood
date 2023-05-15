1e8e65e73857787b8f7df3f500b32d253d189f9e
DiDi0703 <110220867+DiDi0703@users.noreply.github.com>
2023-05-16 01:54:25 +0700

    FE: hoàn thành giao diện shipper

--- TheDuckFood_Customer/app/src/main/java/com/theduckfood/activities/UseCouponActivity.java
+++ TheDuckFood_Customer/app/src/main/java/com/theduckfood/activities/UseCouponActivity.java
@@ -1,8 +1,10 @@
-package com.theduckfood;
+package com.theduckfood.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.theduckfood.R;
 
 public class UseCouponActivity extends AppCompatActivity {
 
