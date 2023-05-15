a4ce2707d255aa922555709314348b914ec26918
Quoc Bao <nguyenquocbaost.2002@gmail.com>
2023-05-15 20:25:20 +0700

    Thêm popup order

--- TheDuckFood_Customer/app/src/main/java/com/theduckfood/adapter/FoodListAdapter.java
+++ TheDuckFood_Customer/app/src/main/java/com/theduckfood/adapter/FoodListAdapter.java
@@ -2,10 +2,13 @@ package com.theduckfood.adapter;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,6 +19,7 @@ import com.theduckfood.R;
 
 import com.theduckfood.databinding.ItemFoodBinding;
 import com.theduckfood.databinding.PopupFoodDetailBinding;
+import com.theduckfood.databinding.PopupOrderBinding;
 import com.theduckfood.model.Food;
 import com.theduckfood.util.Constant;
 import com.theduckfood.util.DateTimeUtil;
@@ -25,9 +29,12 @@ import java.util.List;
 public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.FoodListViewHolder> {
     ItemFoodBinding itemFoodBinding;
     PopupFoodDetailBinding popupFoodDetailBinding;
+    PopupOrderBinding popupOrderBinding;
+    boolean isOrdering = false;
     private Activity context;
     private List<Food> foods;
     Dialog popUpFoodDetail;
+    PopupWindow popUpOrder;
 
     public FoodListAdapter(Activity context, List<Food> foods) {
         this.context = context;
@@ -74,6 +81,9 @@ public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.FoodLi
         popUpFoodDetail.getWindow().setGravity(Gravity.BOTTOM);
         popUpFoodDetail.show();
         loadFoodData(food);
+
+//        View anchor = findViewById(R.id.anchor_view);
+//        popUpOrder.showAtLocation(popupOrderBinding.getRoot(), Gravity.BOTTOM, 0, 0);
     }
 
     private void loadFoodData(Food food) {
@@ -88,13 +98,15 @@ public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.FoodLi
                 .into(popupFoodDetailBinding.imgAvatarFoodPopup);
         popupFoodDetailBinding.txtFoodNamePopup.setText(food.getFoodName());
         popupFoodDetailBinding.txtDescPopup.setText(food.getDescription());
-        popupFoodDetailBinding.txtPricePopup.setText(food.getPrice() + " đ");
+
+        String price  = DateTimeUtil.formatCurrency(String.valueOf(food.getPrice())) + " đ";
+        popupFoodDetailBinding.txtPricePopup.setText(price);
 
         int amount = 1;
         popupFoodDetailBinding.btnIncrease.setOnClickListener(v -> increaseAmount(amount));
         popupFoodDetailBinding.btnDecrease.setOnClickListener(v -> decreaseAmount(amount));
 
-//        popupFoodDetailBinding.btnAdd.setOnClickListener(v -> btnAddClick());
+        popupFoodDetailBinding.btnAdd.setOnClickListener(v -> btnAddClick());
 
     }
 
@@ -114,9 +126,29 @@ public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.FoodLi
     }
 
     private void btnAddClick() {
+        popUpFoodDetail.dismiss();
+
+        showPopUpOrder();
 
     }
 
+    public void showPopUpOrder() {
+        if (isOrdering)
+            return;
+
+        isOrdering = true;
+        View popupView = LayoutInflater.from(context).inflate(R.layout.popup_order, null);
+        popUpOrder = new PopupWindow(popupView);
+        popupOrderBinding = PopupOrderBinding.inflate(LayoutInflater.from(context));
+
+        int margin300SDP = context.getResources().getDimensionPixelSize(com.intuit.sdp.R.dimen._300sdp);
+        int margin10SDP = context.getResources().getDimensionPixelSize(com.intuit.sdp.R.dimen._10sdp);
+
+        popUpOrder.setWidth(margin300SDP);
+        popUpOrder.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        popUpOrder.showAtLocation(popupView, Gravity.BOTTOM, 0, margin10SDP);
+    }
     @Override
     public int getItemCount() {
         return foods.size();
