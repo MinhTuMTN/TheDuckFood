b4afc719cdb3a4b0fcda341870fa6824e176ea60
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2023-05-16 09:42:38 +0700

    Tạo Project cho Shipper

---
+++ TheDuckFoodPartner/app/src/main/java/com/theduckfood/theduckfoodpartner/util/DateTimeUtil.java
@@ -0,0 +1,44 @@
+package com.theduckfood.theduckfoodpartner.util;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateTimeUtil {
+    public static String formatTime(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+        return formatter.format(date);
+    }
+
+    public static String formatDate(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        return formatter.format(date);
+    }
+
+    public static String formatShortDate(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM");
+        return formatter.format(date);
+    }
+
+    public static String formatCurrency(String currency) {
+        NumberFormat formatter = NumberFormat.getInstance(new Locale("en_US"));
+        return formatter.format(Float.parseFloat(currency));
+    }
+
+    public static Date convertStringToDate(String date) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+       return format.parse(date);
+    }
+
+    public static Date getDate(int day, int month, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        return  calendar.getTime();
+    }
+}
